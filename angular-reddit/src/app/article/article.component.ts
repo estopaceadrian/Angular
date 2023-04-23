1526643efa3190c7412803ec7d9fe15c92d5ea6b
estopaceadrian <estopaceadrian@gmail.com>
2023-04-23 14:32:27 +0800

    creating an Article class

---
+++ angular-reddit/src/app/article/article.component.ts
@@ -0,0 +1,27 @@
+import { Component, HostBinding, OnInit } from '@angular/core';
+import { Article } from './article.model';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css'],
+})
+export class ArticleComponent implements OnInit {
+  @HostBinding('attr.class') cssClass = 'row';
+  article: Article;
+  constructor() {
+    this.article = new Article('Angular', 'http://angular.io', 3);
+    this.article = new Article('Fullstack', 'http://fullstack.io', 2);
+    this.article = new Article('Angular Homepage', 'http://angular.io', 1);
+  }
+
+  voteUp() {
+    this.article.voteUp();
+    return false;
+  }
+  voteDown() {
+    this.article.voteDown();
+    return false;
+  }
+  ngOnInit(): void {}
+}
---
+++ angular-reddit/src/app/article/article.model.ts
@@ -0,0 +1,32 @@
+export class Article {
+  title: string;
+  link: string;
+  votes: number;
+
+  constructor(title: string, link: string, votes?: number) {
+    this.title = title;
+    this.link = link;
+    this.votes = votes || 0;
+  }
+
+  voteUp() {
+    this.votes += 1;
+    return false;
+  }
+  voteDown() {
+    this.votes -= 1;
+    return false;
+  }
+  // domain() is  tility function that extracts
+  // the domain from  URL, which we'll explain shortly
+  // domain(): string {
+  //   try {
+  //     // .eg. http://foo.com/path/to/bar
+  //     const domainAndPath: string = this.link.split('//')[1];
+  //     // .eg. foo.com/path/to/bar
+  //     return domainAndPath.split('/')[0];
+  //   } catch (err) {
+  //     // return null;
+  //   }
+  // }
+}
