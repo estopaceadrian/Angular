4e54a75768332ec12932bc7c1f95f579e183fa37
estopaceadrian <estopaceadrian@gmail.com>
2023-05-10 00:09:01 +0800

    created single request

--- searchAngular/src/app/app.module.ts
+++ searchAngular/src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { SimpleHttpComponent } from './simple-http/simple-http.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, SimpleHttpComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
