044f73aa16bcb1f40b7947c67c3960afbd31d631
estopaceadrian <estopaceadrian@gmail.com>
2023-05-11 20:56:25 +0800

    1

--- searchAngular/src/app/simple-http/simple-http.component.ts
+++ searchAngular/src/app/simple-http/simple-http.component.ts
@@ -15,7 +15,7 @@ export class SimpleHttpComponent implements OnInit {
   makeRequest(): void {
     this.loading = true;
     this.http
-      .get('https://jsonplaceholder.typicode.com/posts/1')
+      .get('https://jsonplaceholder.typicode.com/posts/1') //free apis
       .subscribe((data) => {
         this.data = data;
         this.loading = false;
